efef44a52c062acfdb3363bb94fc4e15e05ef6ac
sacheendra <sacheendra.t@gmail.com>
2013-09-10 00:46:48 +0530

    adds working setup.py for py2exe

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 import py2exe
 
-#py2exe_options = dict(
-					#compressed=True,
-					#optimize=2,
-					#excludes=['optparse','doctest','pickle','calendar']
-#					)
+py2exe_options = dict(
+					compressed=True,
+					optimize=2,
+					excludes=['optparse','doctest','pickle','calendar','pdb','inspect','bz2'],
+					dll_excludes=['API-MS-Win-Core-DelayLoad-L1-1-0.dll','API-MS-Win-Core-ErrorHandling-L1-1-0.dll','API-MS-Win-Core-File-L1-1-0.dll','API-MS-Win-Core-Handle-L1-1-0.dll','API-MS-Win-Core-Heap-L1-1-0.dll','API-MS-Win-Core-IO-L1-1-0.dll','API-MS-Win-Core-LibraryLoader-L1-1-0.dll','API-MS-Win-Core-LocalRegistry-L1-1-0.dll','API-MS-Win-Core-Misc-L1-1-0.dll','API-MS-Win-Core-ProcessThreads-L1-1-0.dll','API-MS-Win-Core-Profile-L1-1-0.dll','API-MS-Win-Core-String-L1-1-0.dll','API-MS-Win-Core-Synch-L1-1-0.dll','API-MS-Win-Core-SysInfo-L1-1-0.dll','API-MS-Win-Core-ThreadPool-L1-1-0.dll','API-MS-Win-Security-Base-L1-1-0.dll']
+					)
 
-setup(console=['appbin.py'],#	options={'py2exe':py2exe_options},
+setup(console=['Appbin.py'],
+	options={'py2exe':py2exe_options},
 	data_files=[("config",["config\\appDirs.ini","config\\config.ini"]),
 				("bin",["bin\\7za.exe"]),
 				]
