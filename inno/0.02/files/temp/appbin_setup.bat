7aa1906b89fd174d6c67240423d43140af1ace57
Sagar <sagar.chandarana123@gmail.com>
2013-09-22 02:54:44 +0530

    changed inno for proper git management

--- inno/0.02/files/temp/appbin_setup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo "Setting up Appbin: Wait for a while..."
-"%TEMP%\vcredist_x86_2008.exe" /q:a
-start "Sync Daemon" /D "%APPDATA%\appbin\program_files\"  "%APPDATA%\appbin\program_files\appbin_daemon.exe"
