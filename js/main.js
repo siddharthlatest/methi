b9e2413e1f2909974b6293a3b7f4f32142776e06
Yash Shah <mail@yashshah.com>
2015-07-11 18:03:59 -0500

    Indent and Clean the code

--- js/main.js
+++ js/main.js
@@ -51,9 +51,7 @@ $('.typeahead').typeahead({
   source: engine.ttAdapter(),
   templates: {
       suggestion: function(data){
-        console.log("tEtsing")
         return '<div><h4>' + data.title + '</h4><p> ' + "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui." + '</p></div>';
       }
-    }
-
+  }
 });
