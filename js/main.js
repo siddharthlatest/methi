41cd49354655c2193c76b80e6892ad7c90f09d95
Yash Shah <mail@yashshah.com>
2015-07-11 18:16:29 -0500

    Add Link to the Title of the results

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ $('.typeahead').typeahead({
   source: engine.ttAdapter(),
   templates: {
       suggestion: function(data){
-        return '<div><h4>' + data.title + '</h4><p> ' + "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui." + '</p></div>';
+        return '<div><h4><a href="#">' + data.title + '</a></h4><p> ' + "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui." + '</p></div>';
       }
   }
 });
