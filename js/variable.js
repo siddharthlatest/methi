aa657e0b584d924f41c3b70064650c7c153a5e99
Yash Shah <mail@yashshah.com>
2015-09-16 16:40:52 -0700

    Add new default image

--- js/variable.js
+++ js/variable.js
@@ -35,10 +35,10 @@ function variables(credentials, app_name, index_document_type, method, grid_view
     }
     //this.IMAGE = 'http://d152j5tfobgaot.cloudfront.net/wp-content/uploads/2015/08/yourstory-the-road-to-reinvention-josh-linkner-280x140.jpg';
     //this.IMAGE = 'http://www2.pictures.zimbio.com/gi/Alia+Bhatt+Alia+Bhatt+Portrait+Session+3ukI6nYTRwLl.jpg';
-  this.IMAGE = 'http://d152j5tfobgaot.cloudfront.net/wp-content/uploads/2015/01/YourStory_Transparent-1.png';
-  this.LIST_THUMB = 'images/list_thumb.png';
-  this.GRID_THUMB = 'images/grid_thumb.png';
-  this.SEARCH_THUMB = 'images/search.png';
+  this.IMAGE = 'http://cdn.appbase.io/methi/images/no_image.png';
+  this.LIST_THUMB = 'http://cdn.appbase.io/methi/images/list_thumb.png';
+  this.GRID_THUMB = 'http://cdn.appbase.io/methi/images/grid_thumb.png';
+  this.SEARCH_THUMB = 'http://cdn.appbase.io/methi/images/search.png';
   this.VIEWFLAG = this.GridView;
   this.TAGS = [];
   this.SELECTED_TAGS = [];
